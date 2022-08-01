786788ada3e7a2e9a8072b10ced6c0ddd3ba09d2
510510 <510510@DESKTOP-J32D9DM>
2022-08-01 13:47:45 +0900

    ..

--- main.cpp
+++ main.cpp
@@ -4,16 +4,19 @@
 #include<time.h>
 #include "_kernel.cuh"
 
+#define HALF_EPSILON 0.001
 
-template <typename T>
+template <typename T = __half>
 void printMatrix(const T* matrix, const int row, const int col)
 {
+    std::cout << "\n-------------------------------------------------------------------------------" << std::endl;
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             std::cout << matrix[col * i + j] << " ";
         }
         std::cout << std::endl;
     }
+    std::cout << "-------------------------------------------------------------------------------\n" << std::endl;
 
     return;
 }
@@ -25,8 +28,8 @@ void compareMatrix(const T* A, const T* B, const int row, const int col)
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             auto pos = col * i + j;
-            if (A[pos] != B[pos]) {
-                std::cout << "The tow matrices are different." << std::endl;
+            if (A[pos] - B[pos] > HALF_EPSILON) {     // __half EPSILON
+                std::cout << "Two matrices are different." << std::endl;
                 return;
             }
         }
@@ -38,8 +41,8 @@ void compareMatrix(const T* A, const T* B, const int row, const int col)
 template <typename T = __half>
 void makeSparsity(T* A, const int row, const int col, const double sparsity)
 {
-    int target_nz = static_cast<int>(ceil(row * col * sparsity));     // number of zero in matrix 
-    int matrix_nz = 0;
+    int target_nz = static_cast<int>(ceil(row * col * sparsity)); 
+    int matrix_nz = 0;  // number of zero in matrix
     int i = 0;
     int j = 0;
     int pos;
@@ -51,36 +54,41 @@ void makeSparsity(T* A, const int row, const int col, const double sparsity)
         }
     }   
 
+    // make sparsity
+    int p;
     bool exitOuterLoop = false;
     srand(time(NULL));
     while (true) {
         for (i = 0; i < row; i++) {
-            for (j = 0; j < col; j += (rand() % 4)) {
+            p = 0;
+            for (j = rand() % 4; j < col; j = p * 4 + (rand() % 4)) {
+                p++;
                 pos = col * i + j;
                 if (A[pos] != 0) {
-                    A[pos] = 0.;
-                    matrix_nz += 1;
+                    A[pos] = static_cast<__half>(0.f);
+                    matrix_nz++;
                     if (matrix_nz == target_nz) {
                         exitOuterLoop = true;
                         break;
                     }
                 }
-                if (exitOuterLoop == true)break;
             }
             if (exitOuterLoop == true)break;
         }
         if (exitOuterLoop == true)break;
-    }    
+    }
+    std::cout << "\nA_pruned Sparsity is : " << (double)target_nz / (row * col) << std::endl << std::endl;
 
     return;
 }
 
 
+// A(m x k), B(k x n), C(m x n) --> row-major order
 int main(void)
 {
-    // A(m * k), B(k, n), C(m, n) --> row-major order
+    // input m, n, k
     int m, n, k;
-    std::cout << "input m, n, k" << std::endl;
+    std::cout << "input m, n, k " << std::endl;
     std::cin >> m; std::cin >> n; std::cin >> k;
 
     auto A_size = m * k;
@@ -97,8 +105,8 @@ int main(void)
     __half* csc_hC = (__half*)malloc(C_size * sizeof(__half));           memset(csc_hC, 0, C_size * sizeof(__half));
 
 
-    srand(time(NULL));
-    // fill matrix A, matrix B  0 ~ 9 
+    // fill matrix A, matrix B  0 ~ 9
+    srand(time(NULL)); 
     for (int i = 0; i < A_size; i++)
         hA[i] = static_cast<__half>(static_cast<float>(std::rand() % 10));
     for (int i = 0; i < B_size; i++)
@@ -106,35 +114,44 @@ int main(void)
 
     
     // sparsity 50%
-    std::cout << "sparisty is 50%" << std::endl;
-    cusLtMatmul(hA, hB, lt_hC, hA_pruned, m, n, k);     // --> hA_pruned copy 해온다.
+    std::cout << "\n---------- sparisty is 50% ----------" << std::endl;
+    cusLtMatmul(hA, hB, lt_hC, hA_pruned, m, n, k);     // --> hA_pruned copy 해온다.      //  16배수만 되고 8배수는 안된다.
     cusMatmulCoo(hA_pruned, hB, coo_hC, m, n, k);    
     cusMatmulCsr(hA_pruned, hB, csr_hC, m, n, k);
     cusMatmulCsc(hA_pruned, hB, csc_hC, m, n, k);
-    
+   
+
     // sparsity 75%
-    std::cout << "sparisty is 75%" << std::endl;
+    std::cout << "\n---------- sparisty is 75% ----------" << std::endl;
     makeSparsity(hA_pruned, m, k, 0.75);
+    memset(coo_hC, 0, C_size * sizeof(__half));
+    memset(csr_hC, 0, C_size * sizeof(__half));
+    memset(csc_hC, 0, C_size * sizeof(__half));
     cusMatmulCoo(hA_pruned, hB, coo_hC, m, n, k);
     cusMatmulCsr(hA_pruned, hB, csr_hC, m, n, k);
     cusMatmulCsc(hA_pruned, hB, csc_hC, m, n, k);
-    
+ 
+
     // sparsity 87.5%
-    std::cout << "sparisty is 87.5%" << std::endl;
+    std::cout << "\n---------- sparisty is 87.5% ----------" << std::endl;
     makeSparsity(hA_pruned, m, k, 0.875);
+    memset(coo_hC, 0, C_size * sizeof(__half));
+    memset(csr_hC, 0, C_size * sizeof(__half));
+    memset(csc_hC, 0, C_size * sizeof(__half));
     cusMatmulCoo(hA_pruned, hB, coo_hC, m, n, k);
     cusMatmulCsr(hA_pruned, hB, csr_hC, m, n, k);
     cusMatmulCsc(hA_pruned, hB, csc_hC, m, n, k);
-    
+
+
     // sparsity 99%
-    std::cout << "sparisty is 99%" << std::endl;
+    std::cout << "\n---------- sparisty is 99% ----------" << std::endl;
     makeSparsity(hA_pruned, m, k, 0.99);
+    memset(coo_hC, 0, C_size * sizeof(__half));
+    memset(csr_hC, 0, C_size * sizeof(__half));
+    memset(csc_hC, 0, C_size * sizeof(__half));
     cusMatmulCoo(hA_pruned, hB, coo_hC, m, n, k);
     cusMatmulCsr(hA_pruned, hB, csr_hC, m, n, k);
     cusMatmulCsc(hA_pruned, hB, csc_hC, m, n, k);
-
-
-    // compareMatrix(lt_hC, csc_hC, m, n);
 
 
     // free host memory
